ab0206a0afd0b67b8b7e40ddd4de061189186807
Dharmesh <dharmesh.hemaram@gmail.com>
2024-01-30 20:45:59 +0530

    Add tabs events to content scripts

--- apps/acf-extension/src/background/context-menu.ts
+++ apps/acf-extension/src/background/context-menu.ts
@@ -1,16 +1,10 @@
 import { LOCAL_STORAGE_KEY } from '@dhruv-techapps/acf-common';
-import { ACTION_POPUP, CONTEXT_MENU_CONFIG_PAGE_ID, CONTEXT_MENU_ELEMENT_ID, PERMISSIONS, TABS, getOrigin } from '../common/constant';
-import { NotificationHandler } from './notifications';
+import { ACTION_POPUP, CONTEXT_MENU_CONFIG_PAGE_ID, CONTEXT_MENU_ELEMENT_ID } from '../common/constant';
 import { GoogleAnalytics } from './google-analytics';
-
-const CONTEXT_MENUS_ID = 'context-menu-id';
 export default function registerContextMenus(optionsPageUrl?: string, googleAnalytics?: GoogleAnalytics) {
   chrome.contextMenus.removeAll();
   chrome.contextMenus.create({ id: CONTEXT_MENU_ELEMENT_ID, title: '★ Configure for this Field', contexts: ['page'] });
   chrome.contextMenus.create({ id: ACTION_POPUP, title: '☉ Auto Clicker (Record)', contexts: ['page'] });
-  chrome.contextMenus.create({ id: 'PERMISSION-SEPARATOR', type: 'separator', contexts: ['page'] });
-  chrome.contextMenus.create({ id: PERMISSIONS, title: 'Permissions', contexts: ['page'] });
-  chrome.contextMenus.create({ id: TABS, title: 'Tabs', contexts: ['page'], parentId: PERMISSIONS });
   chrome.contextMenus.create({ id: 'CONFIG-SEPARATOR', type: 'separator', contexts: ['page'] });
   chrome.contextMenus.create({ id: CONTEXT_MENU_CONFIG_PAGE_ID, title: '↗ Open Configuration Page', contexts: ['all'] });
 
@@ -36,24 +30,7 @@ export default function registerContextMenus(optionsPageUrl?: string, googleAnal
             chrome.storage.local.remove([LOCAL_STORAGE_KEY.URL, LOCAL_STORAGE_KEY.XPATH]);
           }
           break;
-        case TABS:
-          if (tab && tab.url) {
-            const origin = getOrigin(tab.url);
-            chrome.permissions.contains({ permissions: ['tabs'], origins: [origin] }, (requested) => {
-              if (!requested) {
-                chrome.permissions.request({ permissions: ['tabs'], origins: [origin] }, (requested) => {
-                  if (requested) {
-                    NotificationHandler.notify(CONTEXT_MENUS_ID, 'Permissions', `tabs permission granted for ${origin}`);
-                  } else {
-                    NotificationHandler.notify(CONTEXT_MENUS_ID, 'Permissions', `tabs permission denied for ${origin}`);
-                  }
-                });
-              } else {
-                NotificationHandler.notify(CONTEXT_MENUS_ID, 'Permissions', `tabs permission granted for ${origin}`);
-              }
-            });
-          }
-          break;
+
         default:
           break;
       }
