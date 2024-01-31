a258458efd07e954b0bbe29444d2a36c1d5e453b
Dharmesh <dharmesh.hemaram@gmail.com>
2024-01-31 22:25:02 +0530

    Updated Blog

--- apps/acf-options-page/src/modal/blog.modal.tsx
+++ apps/acf-options-page/src/modal/blog.modal.tsx
@@ -1,9 +1,8 @@
-import { Alert, Button, Modal, ListGroup } from 'react-bootstrap';
+import { Alert, Button, Modal } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import Markdown from 'react-markdown';
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { blogSelector, hideBlog } from '../store/blog/blog.slice';
-import moment from 'moment';
 
 const BlogModal = () => {
   const { t } = useTranslation();
@@ -17,34 +16,29 @@ const BlogModal = () => {
 
   const convertLink = (body: string) => {
     const regex = /https:\/\/\S+/g;
-    const convertedBody = body.replace(regex, (match) => {
+
+    let convertedBody = body.replace(regex, (match) => {
       const lastSegment = match.split('/').pop();
       return `[${lastSegment}](${match})`;
     });
+
+    convertedBody = convertedBody.replace(`<!-- Release notes generated using configuration in .github/release.yml at ${release?.name} -->`, '');
+
     return convertedBody;
   };
 
   return (
     <Modal show={visible} size='lg' onHide={handleClose} scrollable onShow={onShow} data-testid='blog-modal'>
       <Modal.Header>
-        <Modal.Title as='h3'>{release?.name}</Modal.Title>
+        <Modal.Title as='h3'>
+          <a href={release?.html_url} target='_blank' rel='noreferrer'>
+            {release?.name}
+          </a>
+        </Modal.Title>
       </Modal.Header>
       {release ? (
         <Modal.Body style={{ overflow: 'auto', height: 'calc(100vh - 200px)' }}>
           <Markdown>{convertLink(release.body)}</Markdown>
-          <h3 className='mt-3'>Assets</h3>
-          <ListGroup as='ol'>
-            {release.assets.map((asset) => (
-              <ListGroup.Item as='li' className='d-flex justify-content-between align-items-start'>
-                <div className='me-auto'>
-                  <a href={asset.browser_download_url} target='_blank' rel='noreferrer'>
-                    {asset.name}
-                  </a>
-                </div>
-                <span>{moment(asset.created_at).fromNow()}</span>
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
         </Modal.Body>
       ) : (
         <Alert>Blog content not found</Alert>
