3a1d283c0dbf9c724404f059df3ef82639379112
Dharmesh <dharmesh.hemaram@gmail.com>
2024-03-12 13:36:54 +0530

    added translation for other locales

--- apps/acf-i18n/src/translate.js
+++ apps/acf-i18n/src/translate.js
@@ -1,23 +1,56 @@
-const translate = require('translate-google');
 const fs = require('fs');
+const translate = require('translate-google');
 
-async function translateJson() {
-  // Read the JSON file
-  const data = await fs.promises.readFile('locales/en/web-new.json', 'utf8');
-  const en = JSON.parse(data);
+// Function to recursively translate the values in an object
+async function translateObject(obj, targetLanguage, targetJson) {
+  const translatedObject = {};
 
-  ['fr'].forEach(async (lang) => {
-    translate(en, { to: lang })
-      .then(async (res) => {
-        // Write the translated JSON back to the file
-        const translatedData = JSON.stringify(res, null, 2);
-        await fs.promises.writeFile(`locales/${lang}/web-new.json`, translatedData);
-        //F:\code\auto-clicker-auto-fill\apps\acf-i18n\src\locales\en\web-new.json
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  });
+  for (const key in obj) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (obj.hasOwnProperty(key)) {
+      const value = obj[key];
+      const targetValue = targetJson[key];
+
+      if (typeof value === 'string') {
+        if (targetValue !== undefined && targetValue !== null && targetValue !== '') {
+          translatedObject[key] = targetValue;
+        } else {
+          const translatedValue = await translate(value, { to: targetLanguage });
+          console.log(`Translating "${value}" to "${translatedValue}"`);
+          translatedObject[key] = translatedValue;
+        }
+      } else if (typeof value === 'object') {
+        translatedObject[key] = await translateObject(value, targetLanguage, targetValue);
+      } else {
+        translatedObject[key] = value;
+      }
+    }
+  }
+
+  return translatedObject;
 }
 
-translateJson().catch(console.error);
+// Read the JSON file
+const filePath = 'locales/en/web-new.json';
+
+fs.readFile(filePath, 'utf8', async (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+    return;
+  }
+
+  // Parse the JSON data
+  const englishJson = JSON.parse(data);
+  for (const targetLanguage of ['zh-cn', 'fr', 'ko']) {
+    const translatedFilePath = `locales/${targetLanguage}/web-new.json`;
+
+    const data = await fs.promises.readFile(translatedFilePath, 'utf8');
+    console.error('Processing', targetLanguage);
+    // Parse the JSON data
+    const targetJson = JSON.parse(data);
+
+    // Translate the JSON object and log the result
+    const translatedJson = await translateObject(englishJson, targetLanguage, targetJson);
+    await fs.promises.writeFile(`locales/${targetLanguage}/web-new.json`, JSON.stringify(translatedJson, null, 2));
+  }
+});
