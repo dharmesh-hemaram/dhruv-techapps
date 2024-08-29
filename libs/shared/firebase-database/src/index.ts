b4b1dbbc24bd22fb1ed18aa49d1b131f6181e16f
Dharmesh Patel <dharmesh.hemaram@gmail.com>
2024-08-29 09:41:43 +0530

    Updated firebase database

--- libs/shared/firebase-database/src/index.ts
+++ libs/shared/firebase-database/src/index.ts
@@ -1,3 +1,5 @@
+/// <reference types="chrome"/>
+
 export * from './lib/firebase-database-background';
 export { RUNTIME_MESSAGE_FIREBASE_DATABASE } from './lib/firebase-database.constant';
 export * from './lib/firebase-database.service';
