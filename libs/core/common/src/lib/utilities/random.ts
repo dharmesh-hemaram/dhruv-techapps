aeb7fc09ce67334c8f3c7dda0c95c7d1bf32d523
Dharmesh Patel <dharmesh.hemaram@gmail.com>
2024-10-04 10:36:12 +0530

    Refactor imports in action-table.tsx and remove-configs.modal.tsx

--- libs/core/common/src/lib/utilities/random.ts
+++ libs/core/common/src/lib/utilities/random.ts
@@ -7,12 +7,11 @@ export type RANDOM_UUID = `${string}-${string}-${string}-${string}-${string}`;
 
 export const generateUUID = (): RANDOM_UUID => {
   // Generate a random UUID (version 4)
-  return (
-    crypto.randomUUID() ||
-    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-      const r = (Math.random() * 16) | 0;
-      const v = c === 'x' ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    })
-  );
+  return crypto.randomUUID
+    ? crypto.randomUUID()
+    : ('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        const r = (Math.random() * 16) | 0;
+        const v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }) as RANDOM_UUID);
 };
