ee09fd7f5881af567249f65402a02bcb71505274
Dharmesh Patel <dharmesh.hemaram@gmail.com>
2024-10-04 10:19:08 +0530

    Refactor element-util.ts and common.ts
    - Import generateUUID from @dhruv-techapps/core-common
    - Replace crypto.randomUUID() with generateUUID() for generating UUIDs
    - Remove unused RANDOM_UUID type from common.ts
    - Update imports in action-model.ts and config-model.ts to use generateUUID from @dhruv-techapps/core-common
    - Update imports in notifications.service.ts to use generateUUID from @dhruv-techapps/core-common
    - Update imports in google-analytics-background.ts and sandbox.ts to use generateUUID from @dhruv-techapps/core-common

--- libs/core/common/src/lib/utilities/random.ts
+++ libs/core/common/src/lib/utilities/random.ts
@@ -2,3 +2,17 @@ export const getRandomValues = () => {
   const array = new Uint32Array(1);
   return crypto.getRandomValues(array).toString();
 };
+
+export type RANDOM_UUID = `${string}-${string}-${string}-${string}-${string}`;
+
+export const generateUUID = (): RANDOM_UUID => {
+  // Generate a random UUID (version 4)
+  return (
+    crypto.randomUUID() ||
+    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      const r = (Math.random() * 16) | 0;
+      const v = c === 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    })
+  );
+};
